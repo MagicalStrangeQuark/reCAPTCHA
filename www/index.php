3a20fbf329367f05f36080c27a216e6506b887bb
Wesley Flôres <wesleyfloresterres@gmail.com>
2021-01-04 23:23:03 -0300

    index.php

--- www/index.php
+++ www/index.php
@@ -8,27 +8,29 @@
 
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+  <script src='https://www.google.com/recaptcha/api.js?hl=pt-BR'></script>
 
   <title>Hello, world!</title>
 </head>
 
 <body>
   <div class="container">
-    <form>
+    <form action="submit.php" method="POST">
       <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <input type="email" class="form-control" id="exampleInputEmail1" name="exampleInputEmail1" aria-describedby="emailHelp">
         <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
       </div>
       <div class="mb-3">
         <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
+        <input type="password" class="form-control" id="exampleInputPassword1" name="exampleInputPassword1">
       </div>
       <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1" name="exampleCheck1">
         <label class="form-check-label" for="exampleCheck1">Check me out</label>
       </div>
       <button type="submit" class="btn btn-primary">Submit</button>
+      <div class="g-recaptcha" data-sitekey="6LccFiEaAAAAACk3if_uDGDDtJ15DfuJ7qsy1-2u"></div>
     </form>
   </div>
 
